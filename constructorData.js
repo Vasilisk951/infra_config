7a8c8ff096e9acb061b430387a1af290267870ed
Constantine Medvedev <vasilisk951@gmail.com>
2022-01-19 14:06:25 +0300

    add file readme, updates

---
+++ constructorData.js
@@ -0,0 +1,7 @@
+export class data {
+    constructor(baseUrl, nameProject, secretKey) {
+        this.baseUrl = baseUrl;
+        this.nameProject = nameProject;
+        this.secretKey = secretKey;
+    }
+}
--- index.js
+++ index.js
@@ -1,37 +1,4 @@
-/* 
-    Библиотека для внтренних ресурсов.
-    Она позволяет получать настройки конфигураций из сервиса конфигов.
-
-    Для работы библиотеки необходимо скачать в проект npm пакет env-cmd и изменить команду npm run build в package.json
-        "build": "react-scripts build"  -->  "build": "env-cmd -f .env.development react-scripts build"
-        указывать имя env файла не из примера, а исходя из своих наименований 
-    
-    
-    Для инициализации проекта необходимо вызвать функцию initConfig - она принимает:
-     - baseUrl - адрес без query параметра (type = 'string')
-     - nameProject - имя проекта (type = 'string') 
-     - secretKey - секретный ключ, который был выдан при создании проекта в сервисе конфигов (type = 'string')
-
-     
-     initConfig следует вызвать где нибудь на самом верху проета, в компоненте, которая 100% будет отрендерена(ну или в месте получения конфига вызвать эту функцию)
-
-
-     config - возвращает object с именем и значением конфигурации {name: 'nameConfig', value: 'valueConfig'}
-     config принимает:
-      - nameConfig - имя конфигурации, query параметр для baseUrl (type = 'string')
-      - fn - принмает функцию обновления состояния (hook - React.useState)
-      - nameEnv - запасной вариант если конфигурации в сервисе не окажется. с этим параметром функция вернет значение из локального .env файла (type = 'string')
-        в React имена переменных окружения должны начинаться с REACT_APP
-*/
-
-class data {
-    constructor(baseUrl, nameProject, secretKey) {
-        this.baseUrl = baseUrl;
-        this.nameProject = nameProject;
-        this.secretKey = secretKey;
-    }
-}
-
+import { data } from './constructorData';
 let newData;
 
 export async function config(nameConfig, fn, nameEnv) {
